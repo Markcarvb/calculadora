03cab84c727d372bb205e2a7db5399b7deac093f
Markcarvb <victorassuncao622@gmail.com>
2025-02-17 08:28:20 -0300

    janela criada com ttk-v1

---
+++ Main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
+janela=Tk()
+janela.title("calculadora")
+janela.geometry("235x318")
+
+frame_tela = Frame(janela,width=235, height=50)
+frame_tela.grid(row=0,column=0)
+
+
+janela.mainloop()
