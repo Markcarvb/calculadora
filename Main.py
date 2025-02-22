a33a8bb1f8b2c39b36e28f63e04882495531b2ab
Markcarvb <victorassuncao622@gmail.com>
2025-02-22 18:06:27 -0300

    versao final

--- Main.py
+++ Main.py
@@ -32,7 +32,19 @@ def entradas(event):
 
  #passando valores 
  valor_texto.set(todos_valores)
+
+ #funcao para calculo completo
+def calcular():
+ resultado=eval(todos_valores) 
+ valor_texto.set(str(resultado))
+
+ #funcao limpar item
  
+def limpar_tela():
+    global todos_valores
+    todos_valores=""
+    valor_texto.set("")
+
 #criando Label
 valor_texto=StringVar()
 
@@ -42,7 +54,7 @@ app_label.place(x=0,y=0)
 
 
 #botoes
-b_1= Button(frame_corpo,command=lambda:entradas('C'), text='C', width=11, height=2, bg=cor2, font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
+b_1= Button(frame_corpo,command=limpar_tela, text='C', width=11, height=2, bg=cor2, font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
 b_2= Button(frame_corpo,command=lambda:entradas('%'), text="%", width=5, height=2, bg=cor2, font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_2.place(x=118, y=0)
@@ -81,7 +93,7 @@ b_16= Button(frame_corpo,command=lambda:entradas('0'), text="0", width=11, heigh
 b_16.place(x=0, y=208)
 b_17= Button(frame_corpo,command=lambda:entradas('.'), text=".", width=5, height=2, bg=cor2, font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_17.place(x=118, y=208)
-b_18= Button(frame_corpo,command=lambda:entradas('='), text="=", width=5, height=2,bg=cor5, fg= cor2, font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
+b_18= Button(frame_corpo,command=calcular, text="=", width=5, height=2,bg=cor5, fg= cor2, font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_18.place(x=177, y=208)
 # Pronto
 #botoes estilizados e adicionados
